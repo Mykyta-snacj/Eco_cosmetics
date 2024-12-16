36a27bd48aba2577c6b5da323d85fa28573de6fb
Mykyta Kalniev <nikitamal4ik@gmail.com>
2024-12-16 09:14:39 +0100

    make build

---
+++ index.1a7ba9c5.js
@@ -0,0 +1,2 @@
+document.getElementById("form").addEventListener("submit",function(e){e.preventDefault()}),document.querySelector("form").onsubmit=e=>(e.target.reset(),!1);
+//# sourceMappingURL=index.1a7ba9c5.js.map
